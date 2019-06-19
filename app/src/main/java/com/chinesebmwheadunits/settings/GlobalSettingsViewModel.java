71e9b986fbfde2eed54d22c978f7329cd922f589
Jan-Willem Spuij <jw@spuij.nl>
2019-06-19 12:54:08 +0200

    Wiped native android project.

--- app/src/main/java/com/chinesebmwheadunits/settings/GlobalSettingsViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.chinesebmwheadunits.settings;
-
-import androidx.lifecycle.ViewModel;
-
-public class GlobalSettingsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
