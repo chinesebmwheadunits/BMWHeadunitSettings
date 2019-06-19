8fa5c875a89a7b57190c25d9bbaf06fc41d009b4
Jan-Willem Spuij <jw@spuij.nl>
2019-06-19 17:38:52 +0200

    More tutorial stuff

--- App.tsx
+++ App.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button,StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default function App() {
+
+class HomeScreen extends React.Component {
+  render() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your settings app!</Text>
+      <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
     </View>
   );
+  }
 }
 
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
