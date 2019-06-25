ff711d8a5c1d4f3efec05be6344c0b5aeeb3a87f
Jan-Willem Spuij <jw@spuij.nl>
2019-06-25 13:00:43 +0200

    More state

--- app/components/SettingsSummary.tsx
+++ app/components/SettingsSummary.tsx
@@ -26,23 +26,34 @@ const styles = StyleSheet.create({
       flex: .3
     }
   });
+
+/**
+ * Class with state for the settings screen.
+ */
+ class SettingsSummaryState {
+    taskKillerEnabled: boolean = true;
+    telephoneMuteEnabled: boolean= true;
+    brightnessIntentsEnabled: boolean = true;
+}
   
 /**
  *  Settings summary Component. Displays a summery
  *  on the home screen.
  */
- export default class SettingsSummary extends React.Component {
-    taskKillerEnabled: boolean = true;
-    telephoneMuteEnabled: boolean = true;
-    brightnessIntentsEnabled: boolean = true;
+ export default class SettingsSummary extends React.Component<object, SettingsSummaryState> {
+
+    /**
+     * State.
+     */
+    readonly state = new SettingsSummaryState();
 
     render() {
         return (
             <View style={styles.settingsSummary}>
             <Text style={styles.title}>Chinese BMW head unit settings</Text>
-            <View style={styles.settingsRow}><Text style={styles.settingsText}>Task killer:</Text><Text style={styles.settingsText}>{this.taskKillerEnabled ? 'enabled' : 'disabled'}</Text></View>
-            <View style={styles.settingsRow}><Text style={styles.settingsText}>Telephone mute for navigation:</Text><Text style={styles.settingsText}>{this.telephoneMuteEnabled ? 'enabled' : 'disabled'}</Text></View>
-            <View style={styles.settingsRow}><Text style={styles.settingsText}>Brightness intents:</Text><Text style={styles.settingsText}>{this.brightnessIntentsEnabled ? 'enabled' : 'disabled'}</Text></View>
+            <View style={styles.settingsRow}><Text style={styles.settingsText}>Task killer:</Text><Text style={styles.settingsText}>{this.state.taskKillerEnabled ? 'enabled' : 'disabled'}</Text></View>
+            <View style={styles.settingsRow}><Text style={styles.settingsText}>Telephone mute for navigation:</Text><Text style={styles.settingsText}>{this.state.telephoneMuteEnabled ? 'enabled' : 'disabled'}</Text></View>
+            <View style={styles.settingsRow}><Text style={styles.settingsText}>Brightness intents:</Text><Text style={styles.settingsText}>{this.state.brightnessIntentsEnabled ? 'enabled' : 'disabled'}</Text></View>
           </View>
         );
     }
